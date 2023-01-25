26926d7c29c4e1b1b943bf9eefc3100f257aae6c
jinjuseo <wjbbdq1223@knu.ac.kr>
2023-01-25 13:58:50 +0900

    ABC-LAB_FE_서진주 다시제출

--- getListMultiplesOfTwo.js
+++ getListMultiplesOfTwo.js
@@ -2,13 +2,14 @@ const range = require('range'); // range 모듈을 불러옵니다
 
 function getListMultiplesOfTwo(upTo) {
   // TODO
-  let even=[];
+  /*let even=[];
   for(let i=1;i<=upTo;i++){
     if(i%2===0){
       even.push(i);
     }
   }
-  return even;
+  */
+  return range.range(2,upTo+2,2);
 }
 
 module.exports = getListMultiplesOfTwo;
