ea500a6796a14288863e75114587316fc6ebbe78
kyoongdev <9898junjun2@gmail.com>
2023-01-25 11:48:54 +0900

    kyoongdev : hi

--- getListMultiplesOfTwo.js
+++ getListMultiplesOfTwo.js
@@ -1,7 +1,8 @@
-const range = require('range'); // range 모듈을 불러옵니다
+const range = require("range"); // range 모듈을 불러옵니다
 
 function getListMultiplesOfTwo(upTo) {
   // TODO
+  console.log("hello");
 }
 
 module.exports = getListMultiplesOfTwo;
