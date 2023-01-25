3ba13801253b9c0a5c686de293e84bd9186e5b47
rntjdal <rntjdal98@gmail.com>
2023-01-25 16:01:44 +0900

    01/25/구성미

--- getListMultiplesOfTwo.js
+++ getListMultiplesOfTwo.js
@@ -1,7 +1,11 @@
 const range = require('range'); // range 모듈을 불러옵니다
 
 function getListMultiplesOfTwo(upTo) {
-  // TODO
+  const List=[];
+  for (let i=2; i<=upTo; i+=2) {
+    List.push(i);
+  }
+  return List;
 }
 
 module.exports = getListMultiplesOfTwo;
