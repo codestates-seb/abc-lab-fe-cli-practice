a01ca705644fe751363870941fa606a21648086c
Kim minjeong <minjeongraw@gmail.com>
2023-01-25 18:39:47 +0900

    Kimminjeong

--- getListMultiplesOfTwo.js
+++ getListMultiplesOfTwo.js
@@ -1,6 +1,14 @@
-const range = require('range'); // range 모듈을 불러옵니다
+const range = require("range"); // range 모듈을 불러옵니다
 
 function getListMultiplesOfTwo(upTo) {
+  let result = [];
+
+  for (let i = 2; i <= upTo; i += 2) {
+    result.push(i);
+  }
+
+  return result;
+
   // TODO
 }
 
