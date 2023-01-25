26e3c13b0350747f1f31fbf6b72c7d17b5b69a2e
ImSinYeong <tardiswhoi@naver.com>
2023-01-25 14:31:35 +0900

    임신영 과제

--- getListMultiplesOfTwo.js
+++ getListMultiplesOfTwo.js
@@ -1,7 +1,13 @@
 const range = require('range'); // range 모듈을 불러옵니다
 
 function getListMultiplesOfTwo(upTo) {
-  // TODO
+  let result=[];
+  for(let i=1; i<=upTo; i++){
+    if(i%2===0){
+      result.push(i);
+    }
+  }
+  return result;
 }
 
 module.exports = getListMultiplesOfTwo;
