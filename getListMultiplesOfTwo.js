e36866e159385d557bf05e32dc69e20fd88e72d4
신현중 <sinhyeonjung@sinhyeonjung-ui-MacBookAir.local>
2023-01-25 13:45:34 +0900

    llogin test

--- getListMultiplesOfTwo.js
+++ getListMultiplesOfTwo.js
@@ -2,6 +2,11 @@ const range = require('range'); // range 모듈을 불러옵니다
 
 function getListMultiplesOfTwo(upTo) {
   // TODO
+  const listMultiplesOfTwo = [];
+  for(i=2;i<=upTo;i+=2){ 
+    listMultiplesOfTwo.push(i);
+  }
+  return listMultiplesOfTwo;
 }
 
 module.exports = getListMultiplesOfTwo;
